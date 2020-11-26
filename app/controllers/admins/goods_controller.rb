9dba25b4c6fe619b6384368ecba34ee130e36dfb
EC2 Default User <ec2-user@ip-172-31-30-109.ec2.internal>
2020-11-26 10:38:58 +0000

    [On-Going] AdminsGoodsController

--- app/controllers/admins/goods_controller.rb
+++ app/controllers/admins/goods_controller.rb
@@ -3,17 +3,32 @@ class Admins::GoodsController < ApplicationController
   end
 
   def new
+    @items = Item.new(item_params)
   end
 
   def create
+    @items = Item.new(item_params)
+    if @items.save
+      flash[:notice] ="successfully Created"
+      redirect_to admins_good_path(@item.id)
+    else
+      redirect_to new_admins_good_path
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :image_id, :introduction, :price, :is_actice)
   end
 end
