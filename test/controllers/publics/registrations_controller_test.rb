67cbef29709f72aaba19e07c3c51eac16b96ccc6
EC2 Default User <ec2-user@ip-172-31-30-109.ec2.internal>
2020-12-03 11:16:12 +0000

    [Make] public 新規登録画面

---
+++ test/controllers/publics/registrations_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class Publics::RegistrationsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get publics_registrations_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get publics_registrations_create_url
+    assert_response :success
+  end
+
+end
